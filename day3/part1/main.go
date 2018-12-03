5a2d1a3ebef152635c0608033a1895cec250dcdd
Gergely Brautigam <skarlso777@gmail.com>
2018-12-03 06:38:20 +0100

    bah

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -11,13 +11,29 @@ func main() {
 	filename := os.Args[1]
 	content, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(content), "\n")
-	fabric := make([][][]int, 1000)
+	fabric := make([][][]int, 10)
 	for i := range fabric {
-		fabric[i] = make([][]int, 1000)
+		fabric[i] = make([][]int, 10)
+		for j := range fabric[i] {
+			fabric[i][j] = make([]int, 0)
+		}
 	}
 	// matrix of IDs. if len(id) > 1 -- count it.
 	for _, l := range lines {
 		var id, leftEdge, topEdge, width, heigth int
-		fmt.Sscanf(l, "#%d @ %d,%d: %dx%d", id, leftEdge, topEdge, width, heigth)
+		fmt.Sscanf(l, "#%d @ %d,%d: %dx%d", &id, &leftEdge, &topEdge, &width, &heigth)
+		for i := 0; i < width; i++ {
+			for j := 0; j < heigth; j++ {
+				fabric[i][j] = append(fabric[i][j], id)
+			}
+		}
+	}
+
+	for i := range fabric {
+		for j := range fabric[i] {
+			if len(fabric[i][j]) > 2 {
+				fmt.Println("claimed by more than one.")
+			}
+		}
 	}
 }
