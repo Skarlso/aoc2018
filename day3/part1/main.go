9b7ca3c7e47be90a28d26b3d252ae1129ff4107f
Gergely Brautigam <skarlso777@gmail.com>
2018-12-03 06:26:12 +0100

    Working...

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -10,4 +11,13 @@ func main() {
 	filename := os.Args[1]
 	content, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(content), "\n")
+	fabric := make([][][]int, 1000)
+	for i := range fabric {
+		fabric[i] = make([][]int, 1000)
+	}
+	// matrix of IDs. if len(id) > 1 -- count it.
+	for _, l := range lines {
+		var id, leftEdge, topEdge, width, heigth int
+		fmt.Sscanf(l, "#%d @ %d,%d: %dx%d", id, leftEdge, topEdge, width, heigth)
+	}
 }
