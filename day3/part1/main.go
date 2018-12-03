70aa33a7256faf6b9b0ec6f2b4843aa3374a3e8f
Gergely Brautigam <gergely.brautigam@acquia.com>
2018-12-03 11:30:38 +0100

    wip

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	filename := os.Args[1]
+	size := os.Args[2]
+	s, _ := strconv.Atoi(size)
 	content, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(content), "\n")
-	fabric := make([][][]int, 10)
+	fabric := make([][][]int, s)
 	for i := range fabric {
-		fabric[i] = make([][]int, 10)
+		fabric[i] = make([][]int, s)
 		for j := range fabric[i] {
 			fabric[i][j] = make([]int, 0)
 		}
@@ -22,18 +25,24 @@ func main() {
 	for _, l := range lines {
 		var id, leftEdge, topEdge, width, heigth int
 		fmt.Sscanf(l, "#%d @ %d,%d: %dx%d", &id, &leftEdge, &topEdge, &width, &heigth)
-		for i := leftEdge; i < width; i++ {
-			for j := topEdge; j < heigth; j++ {
+		for i := topEdge; i < width; i++ {
+			for j := leftEdge; j < heigth; j++ {
 				fabric[i][j] = append(fabric[i][j], id)
 			}
 		}
 	}
 
 	for i := range fabric {
+		fmt.Print(".")
 		for j := range fabric[i] {
-			if len(fabric[i][j]) > 2 {
-				fmt.Println("claimed by more than one.")
+			if len(fabric[i][j]) > 1 {
+				fmt.Print("X")
+			} else if len(fabric[i][j]) == 1 {
+				fmt.Print(fabric[i][j][0])
+			} else if len(fabric[i][j]) == 0 {
+				fmt.Print(".")
 			}
 		}
+		fmt.Print("\n")
 	}
 }
