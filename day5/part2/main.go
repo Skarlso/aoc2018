5913c685c1b210e95a06503b6d4de183798862cf
Gergely Brautigam <gergely.brautigam@acquia.com>
2018-12-05 12:00:57 +0100

    Part2 done.

---
+++ day5/part2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	filename := os.Args[1]
+	content, _ := ioutil.ReadFile(filename)
+	ls := make([]int, 0)
+	// could have gathered all the letters from the content, but meh.
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	for _, a := range []rune(alphabet) {
+		c := string(content)
+		c = strings.Replace(c, string(a), "", -1)
+		c = strings.Replace(c, string(unicode.ToUpper(a)), "", -1)
+		runes := []rune(c)
+		reacted := react(runes)
+		ls = append(ls, len(reacted))
+	}
+	sort.Ints(ls)
+	fmt.Println(ls)
+}
+
+func react(runes []rune) []rune {
+	for i := 0; i < len(runes)-1; i++ {
+		curr := runes[i]
+		next := runes[i+1]
+		if curr != next && (unicode.ToUpper(curr) == next || unicode.ToLower(curr) == next) {
+			runes = append(runes[:i], runes[i+2:]...)
+			if i > 0 {
+				i -= 2
+			} else {
+				i--
+			}
+		}
+	}
+	return runes
+}
