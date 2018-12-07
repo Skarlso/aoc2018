f01ffad391ca497d1c27ed8544e6f02a258859ad
Gergely Brautigam <gergely.brautigam@acquia.com>
2018-12-07 09:27:42 +0100

    Day 6 Part 2 done.

--- day6/part2/main.go
+++ day6/part2/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+type coord struct {
+	n        int
+	x        int
+	y        int
+	invalid  bool
+	closesTo int
+}
+
+func (c *coord) String() string {
+	return fmt.Sprintf("x: %d, y: %d, n: %d, invalid: %v, closesTo: %d", c.x, c.y, c.n, c.invalid, c.closesTo)
+}
+
+const (
+	minC      = 0
+	maxC      = 359
+	threshold = 10000
+)
+
+var coords = make([]*coord, 0)
+
+func main() {
+	filename := os.Args[1]
+	content, _ := ioutil.ReadFile(filename)
+	lines := strings.Split(string(content), "\n")
+	for i, l := range lines {
+		var x, y int
+		fmt.Sscanf(l, "%d, %d", &x, &y)
+		c := coord{
+			n:        i,
+			x:        x,
+			y:        y,
+			invalid:  false,
+			closesTo: 0,
+		}
+		coords = append(coords, &c)
+	}
+
+	safeRegionSize := 0
+	for x := 0; x <= maxC; x++ {
+		for y := 0; y <= maxC; y++ {
+			sum := 0
+			for _, c := range coords {
+				sum += abs(c.x-x) + abs(c.y-y)
+			}
+			if sum < threshold {
+				safeRegionSize++
+			}
+		}
+	}
+	fmt.Println(safeRegionSize)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/Skarlso/aoc2018
-
-go 1.12
-
-require go4.org v0.0.0-20181109185143-00e24f1b2599
