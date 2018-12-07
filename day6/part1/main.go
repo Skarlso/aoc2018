dca1808e6cf5c16735bb278e2724f6d8dea8f45d
Gergely Brautigam <skarlso777@gmail.com>
2018-12-07 06:29:25 +0100

    Working day6 part1

--- day6/part1/main.go
+++ day6/part1/main.go
@@ -16,17 +16,16 @@ type coord struct {
 	closesTo int
 }
 
-type mapCoord struct {
-	x int
-	y int
+func (c *coord) String() string {
+	return fmt.Sprintf("x: %d, y: %d, n: %d, invalid: %v, closesTo: %d", c.x, c.y, c.n, c.invalid, c.closesTo)
 }
 
 const (
 	minC = 0
-	maxC = 10
+	maxC = 359
 )
 
-var coords = make([]coord, 0)
+var coords = make([]*coord, 0)
 
 func main() {
 	filename := os.Args[1]
@@ -43,76 +42,52 @@ func main() {
 			invalid:  false,
 			closesTo: 0,
 		}
-		coords = append(coords, c)
+		coords = append(coords, &c)
 	}
+
 	for _, c := range coords {
-		directions := []struct {
-			x int
-			y int
-		}{
-			// {x: 0, y: 0},
-			{x: -1, y: -1},
-			{x: 1, y: 1},
-			{x: 0, y: 1},
-			{x: 0, y: -1},
-			{x: -1, y: 1},
-			{x: 1, y: 0},
-			{x: 1, y: -1},
+		// I could channel this out with go routines.
+		closest, invalid := checkIfClosestInAllDirections(c.x, c.y)
+		if invalid {
+			c.invalid = true
 		}
-		for _, d := range directions {
-			closest, invalid := checkIfClosestInDirection(c.x, c.y, d.x, d.y)
-			if invalid {
-				c.invalid = true
-				// break
-			}
-			c.closesTo += closest
-		}
+		c.closesTo += closest
 		if !c.invalid {
-			c.closesTo++
+			// c.closesTo++
 			areas = append(areas, c.closesTo)
 		}
-		fmt.Printf("Checking: %d; with x: %d, y: %d; closestTo: %d, invalid: %v\n", c.n, c.x, c.y, c.closesTo, c.invalid)
 	}
 	sort.Ints(areas)
 	fmt.Println(areas)
 }
 
-// Checks if this node is closest to that coordinate by looping through
-// the rest of the coordinates and calculating the manhattan distance.
-// If the new coordinate is > max or < min, it means it's an edge node,
-// and extends into infinity therefore, invalid. It needs to includ itself too.
-func checkIfClosestInDirection(startX, startY, dX, dY int) (closest int, invalid bool) {
-	// keep adding the direction until either we hit the "edge" and become invalid
-	// or find someone who is closer to that location.
-	x := startX
-	y := startY
-	for {
-		x += dX
-		y += dY
-		if x <= minC || y <= minC || x >= maxC || y >= maxC {
-			return closest, true
-		}
-		mostClose := false
-		// the problem was that I was matching it to all of them and if it's closest than all of the closest
-		// counted as a +1 to the number. Not just at the end... BAH.
-		myDistance := abs(startX-x) + abs(startY-y)
-		for _, c := range coords {
-			neighbourDistance := abs(c.x-x) + abs(c.y-y)
-			// fmt.Printf("MyDistance: %d; NeighbourDistance: %d\n", myDistance, neighbourDistance)
-			if myDistance < neighbourDistance {
-				mostClose = true
-			} else {
-				mostClose = false
-				break
+func checkIfClosestInAllDirections(startX, startY int) (closest int, invalid bool) {
+	for x := 0; x <= maxC; x++ {
+		for y := 0; y <= maxC; y++ {
+			myDistance := abs(startX-x) + abs(startY-y)
+			mostClose := false
+			for _, c := range coords {
+				if c.x == startX && c.y == startY {
+					continue
+				}
+				neighbourDistance := abs(c.x-x) + abs(c.y-y)
+				if myDistance < neighbourDistance {
+					mostClose = true
+				} else {
+					mostClose = false
+					break
+				}
+			}
+
+			if mostClose {
+				if x <= minC || y <= minC || x >= maxC || y >= maxC {
+					return closest, true
+				}
+				closest++
 			}
 		}
-
-		if mostClose {
-			closest++
-		} else {
-			return closest, false
-		}
 	}
+	return closest, invalid
 }
 
 func abs(a int) int {
