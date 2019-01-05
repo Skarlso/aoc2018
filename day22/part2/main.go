687887da16b23305064eb5fbb5e58a834094351c
Gergely Brautigam <skarlso777@gmail.com>
2019-01-05 10:58:58 +0100

    Working path finding. Now switching to priority queue.

--- day22/part2/main.go
+++ day22/part2/main.go
@@ -1,11 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 type coord struct {
-	x int
-	y int
+	x     int
+	y     int
+	cost  int
+	index int
 }
+
+type pathPrioQueue []*coord
+
+func (pq pathPrioQueue) Len() int { return len(pq) }
+func (pq pathPrioQueue) Less(i, j int) bool {
+	return pq[i].cost < pq[j].cost
+}
+
+func (pq pathPrioQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *pathPrioQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*coord)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *pathPrioQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// // update modifies the priority and value of an Item in the queue.
+// func (pq *pathPrioQueue) update(item *coord, x, y int, cost int) {
+// 	item.x = x
+// 	item.y = y
+// 	item.cost = cost
+// 	heap.Fix(pq, item.index)
+// }
 
 type region struct {
 	geoindex int
@@ -39,55 +82,36 @@ var (
 	// target = coord{x: 14, y: 796}
 	depth  = 510
 	target = coord{x: 10, y: 10}
-	up     = coord{x: 0, y: -1}
-	down   = coord{x: 0, y: +1}
-	left   = coord{x: -1, y: 0}
-	right  = coord{x: 1, y: 0}
+	maxX   = target.x + 10
+	maxY   = target.y + 10
 )
 
 var allPath = make([][]coord, 0)
 
-// func getAllPathToTarget(x, y int) {
-// 	currentPath = make([]coord, 0)
-
-// }
-
-// also
-func neighbours(x, y int, cave [][]region) (path []coord) {
-	// give back all the valid path around the given coordinate
-	// if cave[y+right.y][x+right.x] == '.' || cave[y+right.y][x+right.x] == e {
-	// 	paths = append(paths, coord{x: x + right.x, y: y + right.y})
-	// }
-	// if cave[y+left.y][x+left.x] == '.' || cave[y+left.y][x+left.x] == e {
-	// 	paths = append(paths, coord{x: x + left.x, y: y + left.y})
-	// }
-	// if cave[y+down.y][x+down.x] == '.' || cave[y+down.y][x+down.x] == e {
-	// 	paths = append(paths, coord{x: x + down.x, y: y + down.y})
-	// }
-	// if cave[y+up.y][x+up.x] == '.' || cave[y+up.y][x+up.x] == e {
-	// 	paths = append(paths, coord{x: x + up.x, y: y + up.y})
-	// }
-	if y+right.y < len(cave) {
-		paths = append(paths, coord{x: x + right.x, y: y + right.y})
+func neighbours(c coord, cave [][]region) (paths []coord) {
+	// calculate and add movement cost if switching is needed.
+	// fmt.Println(c)
+	if c.x+1 < len(cave[c.y]) {
+		paths = append(paths, coord{x: c.x + 1, y: c.y})
 	}
-	if cave[y+left.y][x+left.x] == '.' || cave[y+left.y][x+left.x] == e {
-		paths = append(paths, coord{x: x + left.x, y: y + left.y})
+	if c.x-1 > 0 {
+		paths = append(paths, coord{x: c.x - 1, y: c.y})
 	}
-	if cave[y+down.y][x+down.x] == '.' || cave[y+down.y][x+down.x] == e {
-		paths = append(paths, coord{x: x + down.x, y: y + down.y})
+	if c.y+1 < len(cave) {
+		paths = append(paths, coord{x: c.x, y: c.y + 1})
 	}
-	if cave[y+up.y][x+up.x] == '.' || cave[y+up.y][x+up.x] == e {
-		paths = append(paths, coord{x: x + up.x, y: y + up.y})
+	if c.y-1 >= 0 {
+		paths = append(paths, coord{x: c.x, y: c.y - 1})
 	}
 	return
 }
 
 func main() {
 	// Create the map
-	cave := make([][]region, target.y+1)
-	for y := 0; y <= target.y; y++ {
-		cave[y] = make([]region, target.x+1)
-		for x := 0; x <= target.x; x++ {
+	cave := make([][]region, target.y+10)
+	for y := 0; y <= target.y+9; y++ {
+		cave[y] = make([]region, target.x+10)
+		for x := 0; x <= target.x+9; x++ {
 			if y == 0 && x == 0 {
 				cave[y][x] = region{geoindex: 0, t: 0, erosion: 0}
 				continue
@@ -123,38 +147,48 @@ func main() {
 			cave[y][x] = r
 		}
 	}
-	display(cave)
 
 	s := santa{
 		heldTool: torch,
 		pos:      coord{y: 0, x: 0},
 	}
 
-	paths := make([][]coord, 0)
+	// paths := make([][]coord, 0)
 	path := make([]coord, 0)
-	visited := make(map[coord]bool, 0)
+	visited := make(map[coord]coord, 0)
 	goal := target
 	start := s.pos
 	path = append(path, start)
-	visited[start] = true
-	sofar := make([]coord, 0)
+	visited[start] = coord{y: -1, x: -1}
+	// visited[start] = true
+	// sofar := make([]coord, 0)
+	// cost so far
 	for len(path) > 0 {
 		var current coord
 		current, path = path[0], path[1:]
 		if current == goal {
-			paths = append(paths, sofar)
-			sofar = make([]coord, 0)
-			continue
+			// paths = append(paths, sofar)
+			// sofar = make([]coord, 0)
+			break
 		}
-		moves := neighbours()
+		moves := neighbours(current, cave)
 		for _, m := range moves {
 			if _, ok := visited[m]; !ok {
-				visited[m] = true
+				visited[m] = current
 				path = append(path, m)
-				sofar = append(sofar, m)
+				// sofar = append(sofar, m)
 			}
 		}
 	}
+
+	allPath := make([]coord, 0)
+	current := goal
+
+	for current != start {
+		allPath = append(allPath, current)
+		current = visited[current]
+	}
+	displayPath(allPath, cave)
 }
 
 func display(r [][]region) {
@@ -171,4 +205,41 @@ func display(r [][]region) {
 		}
 		fmt.Println()
 	}
+}
+
+func displayPath(path []coord, cave [][]region) {
+	c := color.New(color.FgCyan).Add(color.Underline)
+	for y := 0; y < len(cave); y++ {
+		for x := 0; x < len(cave[y]); x++ {
+			if contains(coord{y: y, x: x}, path) {
+				switch cave[y][x].t {
+				case rocky:
+					c.Print(".")
+				case wet:
+					c.Print("=")
+				case narrow:
+					c.Print("|")
+				}
+			} else {
+				switch cave[y][x].t {
+				case rocky:
+					fmt.Print(".")
+				case wet:
+					fmt.Print("=")
+				case narrow:
+					fmt.Print("|")
+				}
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func contains(c coord, path []coord) bool {
+	for _, v := range path {
+		if v == c {
+			return true
+		}
+	}
+	return false
 }
