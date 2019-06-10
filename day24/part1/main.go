b8cad0867a482e4825f03f3811de9733a04d4103
Gergely Brautigam <skarlso777@gmail.com>
2019-06-10 14:32:14 +0200

    Parsed...

--- day24/part1/main.go
+++ day24/part1/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
+
+	//"github.com/davecgh/go-spew/spew"
 )
+
+type groups []*group
+
+func (a groups) Len() int           { return len(a) }
+func (a groups) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a groups) Less(i, j int) bool { return a[i].EffectivePower > a[j].EffectivePower }
 
 type Army interface {
 	Attack(army *Army)
@@ -14,11 +23,11 @@ type Army interface {
 }
 
 type Infection struct {
-	Groups []*group
+	Groups groups
 }
 
 type ImmuneSystem struct {
-	Groups []*group
+	Groups groups
 }
 
 type unit struct {
@@ -32,7 +41,7 @@ type unit struct {
 }
 
 type group struct {
-	Unit unit
+	Unit *unit
 	EffectivePower int
 }
 
@@ -48,30 +57,99 @@ func main() {
 	immuneSystem.Groups = make([]*group, 0)
 	var format = regexp.MustCompile(`^(\d+) units each with (\d+) hit points (\(?.*\)?)\s?with an attack that does (\d+) (\w+) damage at initiative (\d+)`)
 	for _, l := range lines {
+		if len(l) < 1 || l == "Immune System:" { continue }
 		if l == "Infection:" {
 			infectionsTurn = true
 			continue
 		}
-		if l == "Immune System:" { continue }
-		if infectionsTurn {
+		// 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+		matches := format.FindAllStringSubmatch(l, -1)
+		count, _ := strconv.Atoi(matches[0][1])
+		hitPoint, _ := strconv.Atoi(matches[0][2])
+		weaknessesAndImmunities := matches[0][3]
+		//fmt.Println("WI: ", weaknessesAndImmunities)
+		damage, _ := strconv.Atoi(matches[0][4])
+		attackType := matches[0][5]
+		initiative, _ := strconv.Atoi(matches[0][6])
 
+		g := new(group)
+		u := new(unit)
+		u.initiative = initiative
+		u.hitPoints = hitPoint
+		u.attackDamage = damage
+		u.attackType = attackType
+		u.count = count
+
+		if len(weaknessesAndImmunities) > 0 {
+			weaknessesAndImmunities = strings.ReplaceAll(weaknessesAndImmunities, "(", "")
+			weaknessesAndImmunities = strings.ReplaceAll(weaknessesAndImmunities, ")", "")
+			weaknessesAndImmunities = strings.TrimSpace(weaknessesAndImmunities)
+			if strings.Contains(weaknessesAndImmunities, ";") {
+				wi := strings.Split(weaknessesAndImmunities, ";")
+				var immunities, weaknesses string
+				if strings.Contains(wi[0], "immune") {
+					immunities, weaknesses = wi[0], wi[1]
+				} else {
+					immunities, weaknesses = wi[1], wi[0]
+				}
+				immunities = strings.ReplaceAll(immunities, "immune to", "")
+				weaknesses = strings.ReplaceAll(weaknesses, "weak to", "")
+				immunities = strings.TrimSpace(immunities)
+				weaknesses = strings.TrimSpace(weaknesses)
+				is := strings.Split(immunities, ",")
+				isMap := make(map[string]bool)
+				for _, i := range is {
+					i = strings.TrimSpace(i)
+					isMap[i] = true
+				}
+				ws := strings.Split(weaknesses, ",")
+				wsMap := make(map[string]bool)
+				for _, w := range ws {
+					w = strings.TrimSpace(w)
+					wsMap[w] = true
+				}
+				u.weaknesses = wsMap
+				u.immunities = isMap
+				//fmt.Println(immunities, weaknesses)
+			} else {
+				if strings.Contains(weaknessesAndImmunities, "immune") {
+					immunities := strings.ReplaceAll(weaknessesAndImmunities, "immune to", "")
+					immunities = strings.TrimSpace(immunities)
+					is := strings.Split(immunities, ",")
+					isMap := make(map[string]bool)
+					for _, i := range is {
+						i = strings.TrimSpace(i)
+						isMap[i] = true
+					}
+					u.immunities = isMap
+				} else {
+					weaknesses := strings.ReplaceAll(weaknessesAndImmunities, "weak to", "")
+					weaknesses = strings.TrimSpace(weaknesses)
+					ws := strings.Split(weaknesses, ",")
+					wsMap := make(map[string]bool)
+					for _, w := range ws {
+						w = strings.TrimSpace(w)
+						wsMap[w] = true
+					}
+					u.weaknesses = wsMap
+				}
+
+			}
+		}
+		g.Unit = u
+		g.EffectivePower = u.count * u.attackDamage
+		if infectionsTurn {
+			infection.Groups = append(infection.Groups, g)
 			continue
 		}
-		//var (
-		//	count int
-		//	hitPoint int
-		//	//weakness map[string]bool
-		//	//immunity map[string]bool
-		//	weaknessesAndImmunities string
-		//	damage                  int
-		//	initiative              int
-		//	attackType              string
-		//)
-		// 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+		immuneSystem.Groups = append(immuneSystem.Groups, g)
+	}
+	sort.Sort(immuneSystem.Groups)
+	sort.Sort(infection.Groups)
+	over := false
+	for !over {
 
-		matches := format.FindAllStringSubmatch(l, -1)
-		fmt.Println("Line: ", l)
-		fmt.Printf("Matches: %q\n", matches)
+		over = true
 	}
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Skarlso/aoc2018
 go 1.12
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/fatih/color v1.7.0
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
